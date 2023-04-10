9f386414bb8b9ba5907bf16d654dac42ef0b5abc
Vanessa <vagava.123@gmail.com>
2023-04-10 12:10:28 -0500

    add 101-students.py

---
+++ 0x0D-NoSQL/101-students.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Top students
+"""
+
+
+def top_students(mongo_collection):
+    """
+    Python function that returns all students sorted by average score
+    """
+
+    return mongo_collection.aggregate([
+        { '$project':
+          { 'name': '$name', 'averageScore': { '$avg': '$topics.score' }}},
+        { '$sort': { 'averageScore': -1 }}
+    ])
