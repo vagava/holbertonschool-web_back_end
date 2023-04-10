b852aff731fab4ac911dcee9f855377fe2ef2728
Vanessa <vagava.123@gmail.com>
2023-04-10 12:10:23 -0500

    add 10-update_topics.py

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-""" MongoDB Operations with Python using pymongo """
+"""
+Change school topics
+"""
 
 
 def update_topics(mongo_collection, name, topics):
-    """ Changes all topics of a school document based on the name """
-    query = {"name": name}
-    new_values = {"$set": {"topics": topics}}
-
-    mongo_collection.update_many(query, new_values)
+    """
+    Python function that changes all topics of a school document based on the name
+    """
+    return (mongo_collection.update_many({"name": name},
+            {"$set": {"topics": topics}}))
