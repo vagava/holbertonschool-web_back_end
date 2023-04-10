ddf0a7914966cc61cb094255b9da3a6b5e6a5e4c
Vanessa <vagava.123@gmail.com>
2023-04-10 12:06:33 -0500

    add script_loadproyect.sh

---
+++ 0x0D-NoSQL/script_loadproyect.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ls > Infodir.txt
+cat Infodir.txt | while read y
+do
+    if [[ "$y" != "Infodir.txt" || "$y" != "script_loadproyect.sh" ]];
+    then
+    git add $y
+    git commit -m "add $y"
+    git push origin main
+    # sleep 5m
+    fi
+done
+ 
