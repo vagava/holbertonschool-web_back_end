0be4128f23132c1a98c692ca06361037e66ecc72
Vanessa <vagava.123@gmail.com>
2023-04-10 11:58:53 -0500

    add script_loadproyect.sh

--- 0x0C-MySQL_Advanced/script_loadproyect.sh
+++ 0x0C-MySQL_Advanced/script_loadproyect.sh
@@ -2,7 +2,7 @@
 # ls > Infodir.txt
 cat Infodir.txt | while read y
 do
-    if [[ $y != 'Infodir.txt' || $y != 'script_loadproyect.sh' ]];
+    if [[ "$y" != 'Infodir.txt' || "$y" != 'script_loadproyect.sh' ]];
     then
     git add $y
     git commit -m "add $y"
