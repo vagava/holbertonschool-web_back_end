94f57f76d1389fea46dc82fc929733e26810c2bb
Vanessa <vagava.123@gmail.com>
2023-04-10 12:31:00 -0500

    add 100-await.js

---
+++ 0x0F-ES6_promise/100-await.js
@@ -0,0 +1,17 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let photo = {};
+  let user = {};
+  try {
+    photo = await uploadPhoto();
+    user = await createUser();
+  } catch (e) {
+    photo = null;
+    user = null;
+  }
+  return ({
+    photo,
+    user,
+  });
+}
