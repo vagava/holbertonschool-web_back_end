4d1fcc7484a3cad9ae35c85f477b5a74c6a80222
Vanessa <vagava.123@gmail.com>
2023-04-13 09:10:10 -0500

    add 1-stdin.js

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,15 +1,14 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.setEncoding('utf8');
-console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name !== null) {
-    process.stdout.write(`Your name is: ${name}`);
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
 process.stdin.on('end', () => {
-  if (!(process.stdin.isTTY)) {
-    console.log('This important software is now closing');
-  }
+  process.stdout.write('This important software is now closing\n');
 });
