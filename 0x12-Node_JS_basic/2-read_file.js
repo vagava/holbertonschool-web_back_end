fcf1716876c8e51c44129b83a0726191276b7b7a
Vanessa <vagava.123@gmail.com>
2023-04-13 09:10:22 -0500

    add 2-read_file.js

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -1,27 +1,40 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  let f;
+  let content;
+
   try {
-    f = fs.readFileSync(path, 'utf-8').trim().split('\n');
-  } catch (e) {
-    throw Error('Cannot load the database');
+    content = fs.readFileSync(path);
+  } catch (err) {
+    throw new Error('Cannot load the database');
   }
-  let count = 0;
-  const c = f.slice(1).reduce((p, c) => {
-    count += 1;
-    const fields = c.split(',');
-    if (!p[fields[3]]) {
-      Object.assign(p, {
-        [fields[3]]: [],
-      });
+
+  content = content.toString().split('\n');
+
+  let students = content.filter((item) => item);
+
+  students = students.map((item) => item.split(','));
+
+  const NUMBER_OF_STUDENTS = students.length ? students.length - 1 : 0;
+  console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+
+  const fields = {};
+  for (const i in students) {
+    if (i !== 0) {
+      if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+      fields[students[i][3]].push(students[i][0]);
     }
-    p[fields[3]].push(fields[0]);
-    return p;
-  }, {});
-  console.log(`Number of students: ${count}`);
-  for (const key of Object.keys(c)) {
-    console.log(`Number of students in ${key}: ${c[key].length}. List: ${c[key].join(', ')}`);
+  }
+
+  delete fields.field;
+
+  for (const key of Object.keys(fields)) {
+    console.log(
+      `Number of students in ${key}: ${fields[key].length}. List: ${fields[
+        key
+      ].join(', ')}`,
+    );
   }
 }
 
