079efaf3fba9d8ee3cd391dd3dfb433d847dfe2b
Vanessa <vagava.123@gmail.com>
2023-04-13 09:10:47 -0500

    add 6-http_express.js

--- 0x12-Node_JS_basic/6-http_express.js
+++ 0x12-Node_JS_basic/6-http_express.js
@@ -3,7 +3,12 @@ const express = require('express');
 const app = express();
 const port = 1245;
 
-app.get('/', (req, res) => res.send('Hello Holberton School!'));
-app.listen(port);
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  //   console.log(`Example app listening at http://localhost:${port}`);
+});
 
 module.exports = app;
